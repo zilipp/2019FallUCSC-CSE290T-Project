dcb704986007971fadaf32fc32ae18ca25bdf133
Zi Li <zili100097@gmail.com>
2019-11-19 22:30:31 -0800

    Data cleaning and data preprocessing
    1. Add required packages in requirements.txt
    2. Add data loading and precessing
    
    Signed-off-by: Zi Li <zili100097@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,15 +1,94 @@
+import os
 import logging
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
 
 # self defined module
-from src.utils.utils import init_logger
+from src.utils.utils import init_logger, split_csv
 
 
 # some config values
 _user_logs_file = '..\\out\\logs\\user_logs\\logs.txt'  # User logging directory.
+_data_dir = '..\\data'  # Data directory.
+_num_folds = 10  # Number of folds(k) used in cross validation.'
+_eval_file = 5  # evaluation file in k-fold.
+_num_gpus = 2  # Number of GPUs used.
+
+_vocab_size = 50000  # Number of unique words
+_seq_length = 5000  # Max length of sequence
+
+if _num_gpus == 1:
+    os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+else:
+    os.environ['CUDA_VISIBLE_DEVICES'] = '0, 1'
+
+
+def load_data():
+    # Check k-fold directory
+    logging.info('Checking k-fold files')
+    kfold_dir = os.path.join(_data_dir, 'kfold')
+    if not os.path.exists(kfold_dir):
+        os.mkdir(kfold_dir)
+    if not os.listdir(kfold_dir):
+        split_csv(os.path.join(_data_dir, 'train.csv'), kfold_dir, _num_folds, 'StratifiedShuffleSplit')
+
+    # Load dataset
+    logging.info('Loading train/test dataset')
+    kfold_df = [pd.read_csv(os.path.join(kfold_dir, '{}.csv'.format(i))) for i in range(_num_folds)]
+    test_df = pd.read_csv(os.path.join(_data_dir, 'test.csv'))
+    logging.info('K-fold shape : {0} Test shape : {1}'.format(kfold_df[0].shape, test_df.shape))
+
+    # Split to train and val
+    logging.info('Splitting to train and val')
+    val_df = kfold_df[_eval_file]
+    del kfold_df[_eval_file]
+    train_df = pd.concat(kfold_df)
+    logging.info('Train shape : {0} Eval shape : {1}'.format(train_df.shape, val_df.shape))
+
+    # Assign values
+    logging.info('Assigning values')
+    train_x = train_df['text'].fillna('_na_').values
+    val_x = val_df['text'].fillna('_na_').values
+    test_x = test_df['text'].fillna('_na_').values
+    test_id = test_df['id']
+
+    # Get the target values
+    # id, title, author, text, label
+    logging.info('Get the target values')
+    train_y = train_df['label'].values
+    val_y = val_df['label'].values
+
+    # Release memory
+    del train_df, val_df, test_df
+
+    return train_x, train_y, val_x, val_y, test_x, test_id
+
+
+def preprocess(train_x, val_x, test_x):
+    # Tokenize the sentences
+    logging.info('Tokenizing sentences')
+    tokenizer = keras.preprocessing.text.Tokenizer(num_words=_vocab_size)
+    tokenizer.fit_on_texts(list(train_x))
+    train_x = tokenizer.texts_to_sequences(train_x)
+    val_x = tokenizer.texts_to_sequences(val_x)
+    test_x = tokenizer.texts_to_sequences(test_x)
+
+    # Pad the sentences
+    logging.info('Padding sentences')
+    train_x = keras.preprocessing.sequence.pad_sequences(train_x, maxlen=_seq_length)
+    val_x = keras.preprocessing.sequence.pad_sequences(val_x, maxlen=_seq_length)
+    test_x = keras.preprocessing.sequence.pad_sequences(test_x, maxlen=_seq_length)
+
+    return train_x, val_x, test_x
 
 
 def main():
     init_logger(_user_logs_file)
+    train_x, train_y, val_x, val_y, test_x, test_id = load_data()
+    train_x, val_x, test_x = preprocess(train_x, val_x, test_x)
+
     logging.info('Done!')
 
 
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -2,7 +2,11 @@ import os
 import sys
 import logging
 from logging import handlers
+import pandas as pd
+from sklearn.model_selection import KFold
+from sklearn.model_selection import StratifiedShuffleSplit
 
+_min_seq_len = 50  # Minimum length of sequence
 
 def init_logger(log_file):
     if not os.path.exists(log_file):
@@ -17,3 +21,35 @@ def init_logger(log_file):
     file_handler = logging.handlers.RotatingFileHandler(log_file, maxBytes=(1048576*5), backupCount=7)
     file_handler.setFormatter(output_format)
     logging.getLogger().addHandler(file_handler)
+
+
+def split_csv(file, data_dir, k, method='KFold'):
+    train_df = pd.read_csv(file)
+
+    logging.info("Cleaning data")
+    # Fill "_na_" for title and author column
+    train_df[['title', 'author']] = train_df[['title', 'author']].fillna(value='_na_')
+    # Drop N/A for text column
+    train_df = train_df.dropna()
+    length = []
+    [length.append(len(str(text))) for text in train_df['text']]
+    train_df['length'] = length
+    # dropping the outliers
+    train_df = train_df.drop(train_df['text'][train_df['length'] < _min_seq_len].index, axis=0)
+
+    logging.info("Splitting data")
+    if method == 'KFold':
+        kf = KFold(n_splits=k, shuffle=True, random_state=2018)
+    elif method == 'StratifiedShuffleSplit':
+        kf = StratifiedShuffleSplit(n_splits=k, random_state=2018)
+    else:
+        logging.error('split_csv input unknown method!')
+        assert False
+
+    logging.info("Saving kFold")
+    i = 0
+    for train_index, test_index in kf.split(train_df,
+                                            y=train_df['label'] if method == 'StratifiedShuffleSplit' else None):
+        test_df = train_df.iloc[test_index]
+        test_df.to_csv(os.path.join(data_dir, "{}.csv".format(i)), encoding='utf-8', index=False)
+        i += 1
